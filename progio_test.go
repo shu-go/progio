7e769a9fb5ad5816f56916c7bca76b34a705c5d3
Shuhei Kubota <kubota.shuhei+github@gmail.com>
2018-08-24 17:51:32 +0900

    add test of Throttler

--- progio_test.go
+++ progio_test.go
@@ -2,23 +2,121 @@ package progio_test
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
+	"strings"
 	"testing"
+	"time"
 
 	"bitbucket.org/shu_go/gotwant"
 	"bitbucket.org/shu_go/progio"
 )
 
+type buffer struct {
+	//*os.File
+
+	*bytes.Buffer
+	size int64
+}
+
+func (b buffer) Close() {
+	//b.File.Close()
+}
+
+func (b buffer) Size() int64 {
+	/*
+		stat, _ := b.File.Stat()
+		return stat.Size()
+	*/
+
+	return b.size
+}
+
+func createBuffer() buffer {
+	/*
+		f, err := os.Open("./progio_test.go")
+		if err != nil {
+			panic(err)
+		}
+		return buffer{File: f}
+	*/
+	return buffer{
+		Buffer: bytes.NewBufferString(strings.Repeat("hoge", 100)),
+		size:   4 * 100,
+	}
+}
+
 func TestReader(t *testing.T) {
-	buf := bytes.Buffer{}
-	buf.WriteString("hoge")
-	r := progio.NewReader(&buf, func(p int64) {
-		gotwant.TestExpr(t, p, p <= 4)
+	t.Run("Null", func(t *testing.T) {
+		buf := createBuffer()
+		defer buf.Close()
+
+		r := progio.NewReader(buf, func(p int64) {
+			gotwant.TestExpr(t, p, p <= buf.Size(), gotwant.Desc(fmt.Sprintf("size:%v", buf.Size())))
+		})
+
+		_, err := ioutil.ReadAll(r)
+		gotwant.TestError(t, err, nil)
 	})
 
-	content, err := ioutil.ReadAll(r)
-	gotwant.TestError(t, err, nil)
-	gotwant.Test(t, string(content), "hoge")
+	t.Run("Percent", func(t *testing.T) {
+		buf := createBuffer()
+		defer buf.Close()
+
+		count := 0
+		r := progio.NewReader(
+			buf,
+			func(p int64) {
+				count++
+				gotwant.TestExpr(t, p, 0 <= p)
+				gotwant.TestExpr(t, p, p <= 100)
+			},
+			progio.Percent(buf.Size(), 1),
+		)
+
+		_, err := ioutil.ReadAll(r)
+		gotwant.TestError(t, err, nil)
+
+		gotwant.TestExpr(t, count, 0 < count)
+	})
+
+	t.Run("Time", func(t *testing.T) {
+		buf := createBuffer()
+		defer buf.Close()
+
+		r := progio.NewReader(
+			buf,
+			func(p int64) {
+				gotwant.TestExpr(t, p, p <= buf.Size())
+			},
+			progio.Time(5),
+		)
+
+		_, err := ioutil.ReadAll(r)
+		gotwant.TestError(t, err, nil)
+	})
+
+	t.Run("TimeOver", func(t *testing.T) {
+		buf := createBuffer()
+		defer buf.Close()
+
+		count := 0
+		r := progio.NewReader(
+			buf,
+			func(p int64) {
+				count++
+				if count > 1 {
+					t.Fail()
+				}
+			},
+			progio.Time(5*time.Second),
+		)
+
+		_, err := ioutil.ReadAll(r)
+		gotwant.TestError(t, err, nil)
+
+		gotwant.TestExpr(t, count, count <= 1)
+	})
 }
 
 func TestWriter(t *testing.T) {
